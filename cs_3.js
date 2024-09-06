98fda4928fe76efa0bf2fbb348c61c1497ff9fa1
John Doe <johndoe@email.com>
2024-09-06 02:29:41 -0400

    Create determinePerformanceRating function

--- cs_3.js
+++ cs_3.js
@@ -4,3 +4,19 @@ function calculateAverageSales(sales) {
     let totalSales = sales.reduce((sum, sale) => sum + sale);
     return totalSales / sales.length;
 }
+
+
+
+// Task 2: Create a Function to Determine Performance Rating
+
+function determinePerformanceRating(averageSales) {
+    if (averageSales > 10000) {
+        return "Excellent";
+    } else if (averageSales >= 7000 && averageSales <= 10000) {
+        return "Good";
+    } else if (averageSales >= 4000 && averageSales < 7000) {
+        return "Satisfactory";
+    } else {
+        return "Needs Improvement";
+    }
+}
